7eb81e040cd25a5935282a15755979d523a2bcfc
zmaplex <zmaplex@gmail.com>
2022-01-19 21:50:51 +0800

    add License

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = '0.0.3'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.4'
 
 setup(
     name='fetch_import',
@@ -9,7 +12,10 @@ setup(
     author="zmaplex",
     author_email="zmaplex@gmail.com",
     description="Import Python packages from remote.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/zmaplex/fetch_import",
     packages=find_packages(),
     zip_safe=False,
+    python_requires=">=3.6",
 )
